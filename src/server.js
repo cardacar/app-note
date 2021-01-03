1b9afe6767198275775c104b75ddfb7e2e22ec5e
Carlos Quiros <carlosquiros12@gmail.com>
2021-01-02 22:16:07 -0500

    Agregando las rutas iniciales y estilos a las paginas

--- src/server.js
+++ src/server.js
@@ -28,9 +28,7 @@ app.use(express.urlencoded({extended: false}));//Le dice al servidor cuando lleg
 
 
 //Routes
-app.get('/' ,(req, res)=>{
-    res.render('index');
-});
+app.use(require('./routes/index.routes'));
 
 //Static file
 app.use(express.static(path.join(__dirname + '/public')));
