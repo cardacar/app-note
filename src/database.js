95e6564b53336cf0f98f08f83730e94fac032fcb
Carlos Quiros <carlosquiros12@gmail.com>
2021-01-02 20:48:26 -0500

    Creando modelos para Notes y Users

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 //Importamos las variables creadas en el archivo .env
 const {NOTES_APP_MONGODB_HOST,NOTES_APP_MONGODB_DATABASE} = process.env;
 
-//Creamos el string de conecion con EJS6
+//Creamos el string de conexion con EJS6
 const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
 
 //Conectandome con el uri
---
+++ src/models/user.js
@@ -0,0 +1,24 @@
+//Creando modelo para usuario
+const {Schema, model} = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true}
+},{
+    timestamps: true
+});
+
+//Creamos un metodo que nos encripta la contraseña
+UserSchema.method.encrypPassword = async password =>{
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+};
+
+//Metodo que compara las contraseñas cifradas cuando el usuario inicia session
+UserSchema.method.matchPassword = async function (password){
+    return await bcrypt.compare(password, this.password)
+};
+
+module.exports = model('user', UserSchema);
