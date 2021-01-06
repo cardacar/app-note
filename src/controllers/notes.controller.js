f067845727d6f0551fa7e505ccc28ad048f9c50d
Carlos Quiros <carlosquiros12@gmail.com>
2021-01-05 21:23:20 -0500

    Se divieron las notas dependiendo del usuario y se valida cuando quiere editarla

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -12,7 +12,9 @@ notesCtrl.renderNoteForm = (req, res) =>{
 notesCtrl.createNewNote = async (req, res) =>{
     const {title, description} = req.body;
     const newNote = new Note({title,description});
+    newNote.user = req.user.id;
     await newNote.save();
+
     req.flash('success_msg', 'Note Added Susccesfully');
     res.redirect('/notes');
 }
@@ -20,13 +22,17 @@ notesCtrl.createNewNote = async (req, res) =>{
 //Obtengo las notas y las muestro en /notes
 notesCtrl.renderNotes = async (req, res) =>{
     //Se agrega lean() por problemas entre mongoose y handlebars
-    const notes = await Note.find().lean();
+    const notes = await Note.find({user: req.user.id}).sort({createdAt:'desc'}).lean();
     //envio la variable note a all-notes.hbs
     res.render('notes/all-notes', {notes});
 }
 
 notesCtrl.renderEditForm = async (req,res) =>{
     const note = await Note.findById(req.params.id).lean();
+    if(note.user != req.user.id){
+        req.flash('error_msg', 'No authorized');
+        return res.redirect('/notes');
+    }
     res.render('notes/edit-note', {note});
 }
 
